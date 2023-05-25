d6acedd52786c9c4e8cb622f183081985aebb47d
Momo-87 <momochristian87@gmail.com>
2023-05-25 09:41:08 +0100

    Implementing the counter function with the corresponding test

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,6 +1,6 @@
 // Getting data from the theMealDB API function
 export const getMealData = async () => {
-  const request = new Request('https://themealdb.com/api/json/v1/1/filter.php?a=Canadian');
+  const request = new Request('https://themealdb.com/api/json/v1/1/search.php?f=c');
   const response = await fetch(request);
   const data = await response.json();
   return data.meals;
@@ -13,16 +13,16 @@ export const populatedishes = async (mealData) => {
     const foodCard = document.createElement('div');
     foodCard.classList.add('food-card', 'flex-column');
     foodCard.innerHTML = `<div class = 'image-box'><img src = '${element.strMealThumb}' alt = 'meal'></div>
-                              <div class = 'flex-row meal-title-and-interactions-box'>
-                                <h2 class = 'meal-name'>${element.strMeal}</h2>
-                                <i id ="M${element.idMeal}" class="fa-sharp fa-solid fa-heart heart" style="color: #8b4513;"></i><span id ="L${element.idMeal}" class = "likes">0 likes</span>
-                              </div>
-                              <div class = "buttons-box flex-row">
-                                <button class = 'comment'>Comments</button>
-                                <button class = 'Reservation'>Reservation</button>
-                              </div>
-                              <hr>
-                              `;
+                          <div class = 'flex-row meal-title-and-interactions-box'>
+                            <h2 class = 'meal-name'>${element.strMeal}</h2>
+                            <i id ="M${element.idMeal}" class="fa-sharp fa-solid fa-heart heart" style="color: #8b4513;"></i><span id ="L${element.idMeal}" class = "likes">0 likes</span>
+                          </div>
+                          <div class = "buttons-box flex-row">
+                            <button class = 'comment'>Comments</button>
+                            <button class = 'Reservation'>Reservation</button>
+                          </div>
+                          <hr>
+                          `;
     foodListSection.appendChild(foodCard);
   });
 };
@@ -67,4 +67,18 @@ export const diplayLikes = async (likesData, itemId) => {
       span.textContent = `${element.likes} likes`;
     }
   });
+};
+
+// counter function
+export const counter = async () => {
+  const request = new Request('https://themealdb.com/api/json/v1/1/search.php?f=c');
+  const response = await fetch(request);
+  const data = await response.json();
+  return data.meals.length;
+};
+
+// Display the number of items on the home page function
+export const diplayNumberOfItems = (nbOfItems) => {
+  const li = document.querySelector('li.number-of-items');
+  li.textContent = `Meals(${nbOfItems})`;
 };
